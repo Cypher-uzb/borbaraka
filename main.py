ff43717e00121bc50d8321b20d6e83dc086171a9
YourGitHubUsername <YourEmail@example.com>
2024-11-17 10:02:57 +0500

    v3

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import bot_token
 import logging
+
 logging.basicConfig(level=logging.INFO)
+
 bot = Bot(bot_token)
 dp = Dispatcher(bot)
-Tabnine | Edit | Test | Explain | Document | Ask
+
 @dp.message_handler(commands=['start'])
-async def start_handler(message: Itypes.Message):
-first_name = message.from_user.first_name
-await message.answer (f"Salom {first_name}\n\n\nBot @cypher_uzb tomonidan yoqildi!")
-if_name_ == "_main__":
-executor.start_polling(dp, skip_updates=True)
+async def start_handler(message: types.Message):
+    first_name = message.from_user.first_name  
+    await message.answer(f"Salom {first_name}\n\n\nBot @cypher_uzb tomonidan yoqildi!")
+
+if __name__ == "__main__":  
+    executor.start_polling(dp, skip_updates=True)
